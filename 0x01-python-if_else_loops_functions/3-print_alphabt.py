2cd7c01753843274813346c4acb1619714910f53
Fredrick Owiti <fotsola10@gmail.com>
2021-09-07 12:05:58 -0700

    print alphabet

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for a in range(ord("a"), ord("z") + 1):
+    if chr(a) is not "q" and chr(a) is not "e":
+        print("{:s}".format(chr(a)), end="")#!/usr/bin/python3
+for a in range(ord("a"), ord("z") + 1):
+    if chr(a) is not "q" and chr(a) is not "e":
+        print("{:s}".format(chr(a)), end="")
